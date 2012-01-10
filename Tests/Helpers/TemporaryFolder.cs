c1cd65b0cc899d0cc35de78e367c20826ea5a398
benoit.blanchon@gmail.com <benoit.blanchon@gmail.com>
2012-01-10 22:52:49 +0100

    Fix all file headers

--- Tests/Helpers/TemporaryFolder.cs
+++ Tests/Helpers/TemporaryFolder.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * RecursiveCleaner - Deletes files or folders according to filters defined in XML files.
- * Copyright (C) 2011 Benoit Blanchon
+ * Copyright (C) 2011-2012 Benoit Blanchon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
